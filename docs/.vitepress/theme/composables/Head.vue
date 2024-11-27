5144ebdaa2f631b7d528f367bd1fe113b9ee0119
lemonicy <admin@clashpost.com>
2024-11-27 22:18:05 +0800

    更新：16.654.8 版本安装包

--- docs/.vitepress/theme/composables/Head.vue
+++ docs/.vitepress/theme/composables/Head.vue
@@ -255,12 +255,12 @@ onBeforeUnmount(() => {
         <p>考虑到游戏的现状，经内部研究决定，本项目不再积极维护，这意味着网站的内容更新速度会大幅减慢，且不排除停更的可能性。</p>
         <p><strong>注意：</strong></p>
         <p>1. 安装包功能我们会优先保障，毕竟这个功能不费什么精力，只要有时间我们一定安排。<strong>以后大家仍然可以在这里下载安装包</strong>。</p>
-        <p>2. 数据功能现在只有作者和个别粉丝在维护，更新速度根本快不起来。如果再有人退游且补不上来新人，那就约等于停更了，所以这条公告也有招募维护人员的考虑。</p>
-        <p>3. 除了更新一下场景、载入图这些东西，攻略区事实上已经停更了，不过之前承诺的英雄皮肤页面仍然会做，目前报 404 的页面也会补上，至于新攻略就别指望太多了。</p>
+        <p>2. 数据功能现在只有作者和个别粉丝在维护，更新速度根本快不起来。如果再有人退游且补不上来新人，那就约等于停更了。如果你想成为维护人员，可
+            <a href="https://github.com/lemonicy/clashpost" target="_blank" rel="nofollow noreferrer">前往 Github</a> 做贡献。</p>
+        <p>3. 除了更新一下场景、载入图这些东西，攻略区事实上已经停更，不过之前承诺的英雄皮肤页面仍然会做，目前报 404 的页面也会补上，至于新攻略就别指望太多了。</p>
         <p>4. 之前说的计算功能会砍掉一部分，装备升级计算、英雄升级计算仍然会抽时间做，不过按大本等级分类不准备再做了。</p>
-        <p>5. 如果你愿意陪这个项目走到最后，也可 <a href="https://github.com/lemonicy/clashpost" target="_blank" rel="nofollow noreferrer">前往 Github</a>
-            给本项目续命。另外，在 Github 做操作的时候务必隐藏个人信息，我看有人把自己的 QQ 号都泄露了，还有泄露姓名全拼的。你也不想步那位
-            <a href="https://www.sohu.com/a/724375120_121333743" target="_blank" rel="nofollow noreferrer">承德程序员</a> 的后尘吧（话说这还需要我提醒吗）。</p>
+        <p>5. 在 Github 做操作的时候务必隐藏个人信息，你也不想步那位
+            <a href="https://www.sohu.com/a/724375120_121333743" target="_blank" rel="nofollow noreferrer">承德程序员</a> 的后尘吧。</p>
     </Dialog>
     <Dialog dialogId="cp-search-dialog" title="搜索 (Ctrl + k)" :hasSecondaryBtn="true" :hasPrimaryBtn="true"
         primaryText="开始搜索" secondaryText="关闭窗口" :hasClickPrimaryEvent="true" :hasClickSecondaryEvent="true"
