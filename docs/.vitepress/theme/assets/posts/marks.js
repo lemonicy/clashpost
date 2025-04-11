621aa6751f116abacbc5407eee11f5ab8f38bc49
lemonicy <admin@clashpost.com>
2025-04-11 08:05:23 +0800

    废弃：文章 2754

--- docs/.vitepress/theme/assets/posts/marks.js
+++ docs/.vitepress/theme/assets/posts/marks.js
@@ -1,5 +1,5 @@
 // 已废弃的文章 ID
-export const invalidatedPosts = [20, 66, 268, 295, 559, 596, 689, 756, 801, 1642, 2922, 3048, 4801, 5434];
+export const invalidatedPosts = [20, 66, 268, 295, 559, 596, 689, 756, 801, 1642, 2754, 2922, 3048, 4801, 5434];
 
 // 夜世界 2.0 之前的文章
 export const oldBuilderBasePosts = [349, 848, 1200, 1250, 2763];
