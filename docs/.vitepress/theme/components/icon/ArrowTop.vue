e0f1e13363f6fc3ee3a7af268f3b7e4a1daea775
lemonicy <admin@clashpost.com>
2025-04-19 17:04:48 +0800

    修复：Safari 中部分浏览器无法显示的 bug ------ 其他更新 ------ 删除：项目中已不再使用的 PageFirst 和 PageLast 组件 优化：统一 svg 的代码格式

--- docs/.vitepress/theme/components/icon/ArrowTop.vue
+++ docs/.vitepress/theme/components/icon/ArrowTop.vue
@@ -27,7 +27,7 @@ if (props.propertyKey) {
 </script>
 
 <template>
-    <svg :class="svgDomClass" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+    <svg xmlns="http://www.w3.org/2000/svg" :class="svgDomClass" fill="none" viewBox="0 0 24 24" width="24" height="24" stroke-width="1.5" stroke="currentColor">
         <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 10.5 12 3m0 0 7.5 7.5M12 3v18" />
     </svg>
 </template>
