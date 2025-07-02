4a1e94d6f81282a02b44bd6491f1e0bf49f246d3
lemonicy <admin@clashpost.com>
2025-07-02 14:37:56 -0400

    优化：将 marks.js 中的内容迁移到 global-variables.js 中

--- global-variables.js
+++ global-variables.js
@@ -5,8 +5,19 @@
  * notificationHTML {String} 通知内容，可使用 HTML 代码。如果发布多条通知，通常使用 <hr> 标签分割。
  */
 export const notificationInfo = {
-    "hasNotification": false,
+    "hasNotification": true,
     "notificationCount": 1,
     "currentNotificationId": 8,
-    "notificationHTML": "四月平衡性调整的数据已更新完毕。"
+    "notificationHTML": "网站已上线<strong>部分</strong>六月更新的数据，剩余数据应该可以在 7 月 5 日之前更新完毕。"
 }
+
+/**
+ * 全局文章状态，会在文章加载的时候调用
+ * 
+ * invalidatedPosts 已废弃的文章 ID
+ * oldBuilderBasePosts 夜世界 2.0 之前的文章
+ * maintenancePosts 维护中的文章
+ */
+export const invalidatedPosts = [20, 66, 268, 295, 559, 596, 689, 756, 801, 1642, 2754, 2922, 3048, 4801, 5434];
+export const oldBuilderBasePosts = [349, 848, 1200, 1250, 2763];
+export const maintenancePosts = [];
