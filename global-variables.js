8ad9a33ee294c59070a9e1d898b021ecefad0a4e
lemonicy <admin@clashpost.com>
2025-07-22 05:26:53 -0400

    修改：通知内容

--- global-variables.js
+++ global-variables.js
@@ -1,17 +1,4 @@
 /**
- * hasNotification {Boolean} 是否发布通知，如果填 false 则视为不发布，前端不再运行通知相关的代码，可节省性能。
- * notificationCount {Number} 通知角标显示的数字，默认为 1，只有当同时发布多条通知时才需要修改这个值。
- * currentNotificationId {Number} 当前通知的 ID，前端需要用这个值区分接收到的是全新通知还是以前发布过的通知。发布新通知时请将这个值 +1
- * notificationHTML {String} 通知内容，可使用 HTML 代码。如果发布多条通知，通常使用 <hr> 标签分割。
- */
-export const notificationInfo = {
-    "hasNotification": true,
-    "notificationCount": 1,
-    "currentNotificationId": 8,
-    "notificationHTML": "网站已上线<strong>部分</strong>六月更新的数据，剩余数据应该可以在 7 月 5 日之前更新完毕。"
-}
-
-/**
  * 全局文章状态，会在文章加载的时候调用
  * 
  * invalidatedPosts 已废弃的文章 ID
@@ -21,3 +8,16 @@ export const notificationInfo = {
 export const invalidatedPosts = [20, 66, 268, 295, 559, 596, 689, 756, 801, 1642, 2754, 2922, 3048, 4801, 5434];
 export const oldBuilderBasePosts = [349, 848, 1200, 1250, 2763];
 export const maintenancePosts = [];
+
+/**
+ * hasNotification {Boolean} 是否发布通知，如果填 false 则视为不发布，前端不再运行通知相关的代码，可节省性能。
+ * notificationCount {Number} 通知角标显示的数字，默认为 1，只有当同时发布多条通知时才需要修改这个值。
+ * currentNotificationId {Number} 当前通知的 ID，前端需要用这个值区分接收到的是全新通知还是以前发布过的通知。发布新通知时请将这个值 +1
+ * notificationHTML {String} 通知内容，可使用 HTML 代码。如果发布多条通知，通常使用 <hr> 标签分割。
+ */
+export const notificationInfo = {
+    "hasNotification": true,
+    "notificationCount": 1,
+    "currentNotificationId": 8,
+    "notificationHTML": "网站已上线六月更新的数据（精制建筑除外）。"
+}
