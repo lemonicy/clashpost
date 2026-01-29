f29f5dd755cf18a1c1b552204db73f2f41cd6ab0
lemonicy <admin@clashpost.com>
2026-01-29 06:11:32 -0500

    优化：给可用的临时单位增加横幅提示

--- docs/.vitepress/theme/customized/upgrade/UpgradeBanner.vue
+++ docs/.vitepress/theme/customized/upgrade/UpgradeBanner.vue
@@ -11,7 +11,9 @@ const props = defineProps({
 });
 
 const link = props.link;
-let tempUnitAvailable = false;
+let tempTroopAvailable = false;
+let tempSpellAvailable = false;
+let tempTrapAvailable = false;
 let craftedBuildingAvailable = false;
 
 if (link && isUpgradeDetails(link)) {
@@ -19,7 +21,23 @@ if (link && isUpgradeDetails(link)) {
     const unitId = link.substring(9, 13);
 
     // 判断是否为可用的临时单位
-    tempUnitAvailable = availableTempUnits.includes(unitId);
+    let availableTempTroops = [];
+    let availableTempSpells = [];
+    let availableTempTraps = [];
+    // 判断该临时单位是兵种、法术还是陷阱，并存入对应的分类中
+    for (const availableItem of availableTempUnits) {
+        const availableItemFirst3 = availableItem.substring(0, 3); // 序号中的前三个字符
+        if (["0e0", "0e1", "0e2", "0e3", "0e4", "0e5", "0e6", "0e7"].includes(availableItemFirst3)) {
+            availableTempTroops.push(availableItem);
+        } else if (["0e8", "0e9", "0ea", "0eb", "0ec", "0ed", "0ee", "0ef"].includes(availableItemFirst3)) {
+            availableTempSpells.push(availableItem);
+        } else {
+            availableTempTraps.push(availableItem);
+        }
+    }
+    tempTroopAvailable = availableTempTroops.includes(unitId);
+    tempSpellAvailable = availableTempSpells.includes(unitId);
+    tempTrapAvailable = availableTempTraps.includes(unitId);
 
     // 判断是否为可用的精工防御
     craftedBuildingAvailable = availableCraftedBuildings.includes(unitId);
@@ -27,8 +45,14 @@ if (link && isUpgradeDetails(link)) {
 </script>
 
 <template>
-    <Callout type="success" class="cp-callout-available-tip" v-if="tempUnitAvailable">
-        该临时单位现在可用。在活动期间，你可以使用临时单位。
+    <Callout type="success" class="cp-callout-available-tip" v-if="tempTroopAvailable">
+        该临时兵种现在可用。在活动期间，你可以训练和使用该兵种。
+    </Callout>
+    <Callout type="success" class="cp-callout-available-tip" v-if="tempSpellAvailable">
+        该临时法术现在可用。在活动期间，你可以配置和部署该法术。
+    </Callout>
+    <Callout type="success" class="cp-callout-available-tip" v-if="tempTrapAvailable">
+        该临时陷阱现在可用。在活动期间，你可以购买和部署该陷阱。
     </Callout>
     <Callout type="success" class="cp-callout-available-tip" v-if="craftedBuildingAvailable">
         该精工防御现在可用。在新一批精工防御推出之前，你可以升级该防御。
